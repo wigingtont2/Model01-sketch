7a4a126900ad02e5556ce90243f70d6e4c72e32c
Gergely Nagy <algernon@madhouse-project.org>
2017-02-26 09:59:42 +0100

    Make LEDs controllable via Focus
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/Focus.cpp
+++ src/Focus.cpp
@@ -17,14 +17,119 @@
  */
 
 #include "Focus.h"
+#include <Kaleidoscope-LEDControl.h>
 
 namespace algernon {
   namespace FocusSetup {
+    static bool led (const char *command) {
+      enum {
+        GET,
+        SET,
+        SETALL,
+        OFF,
+        NEXTMODE,
+        MODE,
+      } subCommand;
+
+      if (strncmp_P (command, PSTR ("led."), 4) != 0)
+        return false;
+      if (strcmp_P (command + 4, PSTR ("get")) == 0)
+        subCommand = GET;
+      else if (strcmp_P (command + 4, PSTR ("set")) == 0)
+        subCommand = SET;
+      else if (strcmp_P (command + 4, PSTR ("setAll")) == 0)
+        subCommand = SETALL;
+      else if (strcmp_P (command + 4, PSTR ("off")) == 0)
+        subCommand = OFF;
+      else if (strcmp_P (command + 4, PSTR ("nextMode")) == 0)
+        subCommand = NEXTMODE;
+      else if (strcmp_P (command + 4, PSTR ("mode")) == 0)
+        subCommand = MODE;
+      else
+        return false;
+
+      switch (subCommand) {
+      case GET:
+        {
+          uint8_t idx = Serial.parseInt ();
+          cRGB c = LEDControl.led_get_crgb_at (idx);
+          const __FlashStringHelper *spc = F(" ");
+
+          Serial.print (c.r);
+          Serial.print (spc);
+          Serial.print (c.g);
+          Serial.print (spc);
+          Serial.println (c.b);
+          break;
+        }
+      case SET:
+        {
+          uint8_t idx = Serial.parseInt ();
+          cRGB c;
+
+          c.r = Serial.parseInt ();
+          c.g = Serial.parseInt ();
+          c.b = Serial.parseInt ();
+
+          LEDControl.led_set_crgb_at (idx, c);
+          break;
+        }
+      case SETALL:
+        {
+          cRGB c;
+
+          c.r = Serial.parseInt ();
+          c.g = Serial.parseInt ();
+          c.b = Serial.parseInt ();
+
+          LEDControl.set_all_leds_to (c);
+
+          break;
+        }
+      case OFF:
+        LEDControl.set_all_leds_to (0, 0, 0);
+        break;
+      case NEXTMODE:
+        LEDControl.next_mode ();
+        break;
+      case MODE:
+        {
+          uint8_t mode = Serial.parseInt ();
+
+          LEDControl.set_mode (mode);
+          break;
+        }
+      }
+
+      Serial.read ();
+      return true;
+    }
+
     void configure (void) {
       USE_PLUGINS (&Focus);
 
       Focus.addHook (FOCUS_HOOK_HELP);
       Focus.addHook (FOCUS_HOOK_VERSION);
+      Focus.addHook (FOCUS_HOOK (led,
+                                 "led.set index r g b\n"
+                                 "-------------------\n"
+                                 "Set the led at `index` to the color described with `r`, `g`, and `b`.\n\n"
+                                 "led.setAll r g b\n"
+                                 "----------------\n"
+                                 "Set all leds to the color described with `r`, `g`, and `b`.\n\n"
+                                 "led.get index\n"
+                                 "-------------\n"
+                                 "Return the color of the LED at the `index` position, in `r g b` format.\n\n"
+                                 "led.nextMode\n"
+                                 "------------\n"
+                                 "Moves on to the next LED effect mode.\n\n"
+                                 "led.mode N\n"
+                                 "----------\n"
+                                 "Switches to LED effect mode `n`.\n\n"
+                                 "led.off\n"
+                                 "-------\n"
+                                 "Turn all LEDs off."));
+
     }
   };
 };
