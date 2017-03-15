dc735f415a5aa639a90796739bd38889fe0dec95
Gergely Nagy <algernon@madhouse-project.org>
2017-03-15 17:20:51 +0100

    Lift out the keymap into a header
    Since the keymap can now live in EEPROM, lift out the keymap into a separate
    header, and disable it. It's still there as a reference, in case the EEPROM
    would need to be rewritten, but it is not compiled into the firmware anymore.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ src/keymap.h
@@ -0,0 +1,179 @@
+/* -*- mode: c++ -*-
+ * Model01-Sketch -- algernon's Model01 Sketch
+ * Copyright (C) 2016, 2017  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define WITH_PROGMEM_KEYMAP 0
+
+using namespace KaleidoscopePlugins::LangPack;
+
+#define R(n) (Key){.raw = n}
+#define MW(d) Key_mouseWarp ## d
+#define MM(d) Key_mouse ## d
+
+enum {
+  APPSEL_MUSIC,
+  APPSEL_CHAT,
+  APPSEL_EMACS,
+  APPSEL_TERM,
+  APPSEL_WEB,
+
+  MSP,
+  MSM,
+};
+
+#define Key_AT    LSHIFT(Key_2)
+#define Key_STAR  LSHIFT(Key_8)
+#define Key_DOLLR LSHIFT(Key_4)
+#define Key_CARET LSHIFT(Key_6)
+#define Key_PRCNT LSHIFT(Key_5)
+#define Key_EXCLM LSHIFT(Key_1)
+#define Key_HASH  LSHIFT(Key_3)
+#define Key_AND   LSHIFT(Key_7)
+#define Key_LCB   LSHIFT(Key_LBracket)
+#define Key_RCB   LSHIFT(Key_RBracket)
+
+#define AS_MUS M(APPSEL_MUSIC)
+#define AS_CHT M(APPSEL_CHAT)
+#define AS_ECS M(APPSEL_EMACS)
+#define AS_TRM M(APPSEL_TERM)
+#define AS_WEB M(APPSEL_WEB)
+
+#define MS_SUP MM(ScrollUp)
+#define MS_SDN MM(ScrollDn)
+
+#if WITH_PROGMEM_KEYMAP
+const Key keymaps[][ROWS][COLS] PROGMEM = {
+  [_DVORAK] = KEYMAP_STACKED
+  (
+    TD(F11)       ,Key_LCB   ,Key_AT    ,Key_STAR   ,Key_DOLLR ,Key_CARET ,TD(TMUX)
+   ,Key_Backtick  ,Key_Quote ,Key_Comma ,Key_Period ,Key_P     ,Key_Y     ,TD(LPB)
+   ,Key_Tab       ,Key_A     ,Key_O     ,Key_E      ,Key_U     ,Key_I
+   ,Key_playPause ,Key_Slash ,Key_Q     ,Key_J      ,Key_K     ,Key_X     ,OSM(LCtrl)
+
+   ,TD(COLON) ,Key_Backspace ,OSM(LShift) ,Key_Esc
+   ,OSL(_NAV)
+
+   ,TD(TMUXP) ,Key_PRCNT ,Key_EXCLM ,Key_HASH ,Key_AND ,Key_RCB ,TD(MNP)
+   ,TD(RPB)   ,Key_F     ,Key_G     ,Key_C    ,Key_R   ,Key_L   ,Key_Backslash
+              ,Key_D     ,Key_H     ,Key_T    ,Key_N   ,Key_S   ,Key_Equals
+   ,OSM(LAlt) ,Key_B     ,Key_M     ,Key_W    ,Key_V   ,Key_Z   ,LEAD(MAIN)
+
+   ,TD(GUI) ,Key_Enter ,Key_Space ,Key_Minus
+   ,OSL(_AUX)
+  ),
+
+  [_ADORE] = KEYMAP_STACKED
+  (
+    TD(F11)       ,Key_LCB ,Key_AT ,Key_STAR  ,Key_DOLLR ,Key_CARET  ,TD(TMUX)
+   ,Key_Backslash ,Key_X   ,Key_W  ,Key_C     ,Key_H     ,Key_F      ,TD(LPB)
+   ,Key_Tab       ,Key_A   ,Key_O  ,Key_E     ,Key_I     ,Key_U
+   ,Key_playPause ,Key_Z   ,Key_Q  ,Key_Quote ,Key_Comma ,Key_Period ,OSM(LCtrl)
+
+   ,TD(COLON) ,Key_Backspace ,OSM(LShift) ,Key_Esc
+   ,OSL(_NAV)
+
+   ,TD(TMUXP) ,Key_PRCNT ,Key_EXCLM ,Key_HASH ,Key_AND ,Key_RCB   ,TD(MNP)
+   ,TD(RPB)   ,Key_M     ,Key_G     ,Key_L    ,Key_P   ,Key_Slash ,Key_Backslash
+              ,Key_D     ,Key_R     ,Key_T    ,Key_N   ,Key_S     ,Key_Equals
+   ,OSM(LAlt) ,Key_B     ,Key_K     ,Key_V    ,Key_Y   ,Key_J     ,LEAD(MAIN)
+
+   ,Key_LGUI ,Key_Enter ,Key_Space ,Key_Minus
+   ,OSL(_AUX)
+  ),
+
+  [_NAV] = KEYMAP_STACKED
+  (
+    ___ ,Key_F9 ,Key_F7 ,Key_F5 ,Key_F3 ,Key_F1 ,XXX
+   ,XXX ,MS_SUP ,MM(Up) ,MS_SDN ,MW(NW) ,MW(NE) ,MW(End)
+   ,XXX ,MM(L)  ,MM(Dn) ,MM(R)  ,MW(SW) ,MW(SE)
+   ,XXX ,XXX    ,XXX    ,XXX    ,XXX    ,XXX    ,___
+
+   ,TD(VOLD) ,Key_volumeUp ,___ ,___
+   ,___
+
+   ,LGUI(Key_L) ,Key_F10 ,Key_F2     ,Key_F4      ,Key_F6     ,Key_F8 ,XXX
+   ,XXX         ,M(MSP)  ,Key_Home   ,Key_UpArrow ,Key_End    ,XXX    ,XXX
+                ,M(MSM)  ,Key_LArrow ,Key_DnArrow ,Key_RArrow ,XXX    ,XXX
+   ,___         ,XXX     ,Key_PageUp ,XXX         ,Key_PageDn ,XXX    ,XXX
+
+   ,MM(BtnL) ,___ ,___ ,MM(BtnR)
+   ,OSL(_EMPTY)
+  ),
+
+  [_AUX] = KEYMAP_STACKED
+  (
+    XXX ,XXX       ,XXX        ,XXX       ,XXX        ,XXX       ,XXX
+   ,XXX ,XXX       ,R(HUN_ODA) ,XXX       ,R(HUN_UDA) ,XXX       ,XXX
+   ,XXX ,R(HUN_AA) ,R(HUN_OA)  ,R(HUN_EA) ,R(HUN_UA)  ,R(HUN_IA)
+   ,___ ,XXX       ,R(HUN_OU)  ,XXX       ,R(HUN_UU)  ,XXX       ,XXX
+
+   ,XXX ,___ ,___ ,___
+   ,OSL(_EMPTY)
+
+   ,XXX ,XXX   ,XXX   ,XXX   ,XXX   ,XXX ,XXX
+   ,XXX ,XXX   ,Key_7 ,Key_8 ,Key_9 ,XXX ,XXX
+        ,Key_0 ,Key_4 ,Key_5 ,Key_6 ,XXX ,XXX
+   ,___ ,XXX   ,Key_1 ,Key_2 ,Key_3 ,XXX ,XXX
+
+   ,XXX ,XXX ,XXX ,XXX
+   ,___
+  ),
+
+  [_APPSEL] = KEYMAP_STACKED
+  (
+    XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX
+   ,XXX
+
+   ,XXX ,XXX ,XXX    ,XXX    ,XXX    ,XXX ,XXX
+   ,XXX ,XXX ,XXX    ,XXX    ,XXX    ,XXX ,XXX
+        ,XXX ,AS_TRM ,AS_ECS ,AS_WEB ,XXX ,XXX
+   ,XXX ,XXX ,AS_MUS ,XXX    ,AS_CHT ,XXX ,XXX
+
+   ,XXX ,XXX ,XXX ,XXX
+   ,XXX
+  ),
+
+#if 0
+  // This is here for reference only, this layer is implemented in the getKey
+  // function instead.
+  [_EMPTY] = KEYMAP_STACKED
+  (
+    XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX
+   ,___
+
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+        ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+   ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX ,XXX
+
+   ,XXX ,XXX ,XXX ,XXX
+   ,___
+  ),
+#endif
+};
+#else
+const Key keymaps[][ROWS][COLS] PROGMEM = {
+};
+#endif
