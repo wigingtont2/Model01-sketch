e7b7f57645654e017fbe7d3817cd1f46cd471103
Gergely Nagy <algernon@madhouse-project.org>
2017-06-01 11:43:59 +0200

    Colormap: Drop the PROGMEM Colormap support
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/Colormap.cpp
+++ src/Colormap.cpp
@@ -18,187 +18,22 @@
 
 #include "00-config.h"
 
-#if WITH_PROGMEM_COLORMAP
-#  include <Kaleidoscope-Colormap.h>
-#else
-#  include <Kaleidoscope-EEPROM-Colormap.h>
-#  include <Kaleidoscope-Focus.h>
-#endif
+#include <Kaleidoscope-EEPROM-Colormap.h>
+#include <Kaleidoscope-Focus.h>
 
 #include "Layers.h"
 
 namespace algernon {
   namespace Colormap {
 
-#if WITH_PROGMEM_COLORMAP
-    // Colors
-    enum {
-      OFF,
-      FAINT_RED,
-      FAINT_GREEN,
-      FAINT_BLUE,
-      CYAN,
-      RED,
-      BLUE,
-      NAVY,
-      YELLOW,
-      GREEN,
-      PINK,
-      MGNT,
-
-      TRNS = KaleidoscopePlugins::ColormapEffect::Transparent,
-    };
-
-    static const cRGB colors[] PROGMEM = {
-      [OFF]         = {0x00, 0x00, 0x00},
-      [FAINT_RED]   = {0x00, 0x00, 0x40},
-      [FAINT_GREEN] = {0x00, 0x40, 0x00},
-      [FAINT_BLUE]  = {0x40, 0x00, 0x00},
-      [CYAN]        = {0xff, 0x88, 0x00},
-      [RED]         = {0x00, 0x00, 0xff},
-      [BLUE]        = {0xff, 0x00, 0x00},
-      [NAVY]        = {0x80, 0x00, 0x00},
-      [YELLOW]      = {0x00, 0xff, 0xff},
-      [GREEN]       = {0x00, 0xff, 0x00},
-      [PINK]        = {0x80, 0x80, 0xff},
-      [MGNT]        = {0xcc, 0x33, 0xff},
-    };
-
-    static const uint8_t colorMap[LAYER_MAX][ROWS][COLS] PROGMEM = {
-      [_DVORAK] = KEYMAP_STACKED
-      (
-        OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,FAINT_BLUE
-
-       ,OFF ,OFF ,FAINT_RED ,OFF
-       ,CYAN
-
-       ,OFF         ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF         ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-                    ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,FAINT_GREEN ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-
-       ,OFF ,OFF ,OFF ,OFF
-       ,RED
-       ),
-
-      [_ADORE] = KEYMAP_STACKED
-      (
-        OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,FAINT_BLUE
-
-       ,OFF ,OFF ,FAINT_RED ,OFF
-       ,CYAN
-
-       ,OFF         ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF         ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-                    ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,FAINT_GREEN ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-
-       ,OFF ,OFF ,OFF ,OFF
-       ,RED
-       ),
-
-      [_NAV] = KEYMAP_STACKED
-      (
-        CYAN ,CYAN   ,CYAN  ,CYAN   ,CYAN ,CYAN ,TRNS
-       ,TRNS ,YELLOW ,GREEN ,YELLOW ,PINK ,PINK ,PINK
-       ,TRNS ,GREEN  ,GREEN ,GREEN  ,PINK ,PINK
-       ,TRNS ,TRNS   ,TRNS  ,TRNS   ,TRNS ,TRNS ,TRNS
-
-       ,GREEN ,GREEN ,TRNS ,TRNS
-       ,TRNS
-
-       ,PINK ,CYAN   ,CYAN ,CYAN ,CYAN ,CYAN ,TRNS
-       ,TRNS ,YELLOW ,MGNT ,BLUE ,MGNT ,TRNS ,TRNS
-             ,YELLOW ,BLUE ,BLUE ,BLUE ,TRNS ,TRNS
-       ,TRNS ,TRNS   ,MGNT ,TRNS ,MGNT ,TRNS ,TRNS
-
-       ,MGNT ,TRNS ,TRNS ,MGNT
-       ,TRNS
-       ),
-
-      [_AUX] = KEYMAP_STACKED
-      (
-        TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS ,TRNS ,RED  ,TRNS ,RED  ,TRNS ,TRNS
-       ,TRNS ,RED  ,RED  ,RED  ,RED  ,RED
-       ,TRNS ,TRNS ,RED  ,TRNS ,RED  ,TRNS ,TRNS
-
-       ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS
-
-       ,TRNS ,TRNS  ,TRNS  ,TRNS  ,TRNS  ,TRNS ,TRNS
-       ,TRNS ,TRNS  ,GREEN ,GREEN ,GREEN ,TRNS ,TRNS
-             ,GREEN ,GREEN ,GREEN ,GREEN ,TRNS ,TRNS
-       ,TRNS ,TRNS  ,GREEN ,GREEN ,GREEN ,TRNS ,TRNS
-
-       ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS
-       ),
-
-      [_APPSEL] = KEYMAP_STACKED
-      (
-        TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS ,TRNS
-
-       ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS
-
-       ,TRNS ,TRNS ,TRNS   ,TRNS   ,TRNS   ,TRNS ,TRNS
-       ,TRNS ,TRNS ,TRNS   ,TRNS   ,TRNS   ,TRNS ,TRNS
-             ,TRNS ,YELLOW ,YELLOW ,YELLOW ,TRNS ,TRNS
-       ,TRNS ,TRNS ,YELLOW ,TRNS   ,YELLOW ,TRNS ,TRNS
-
-       ,TRNS ,TRNS ,TRNS ,TRNS
-       ,TRNS
-       ),
-
-      [_EMPTY] = KEYMAP_STACKED
-      (
-        OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-
-       ,OFF ,OFF ,OFF ,OFF
-       ,OFF
-
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-            ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-       ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF ,OFF
-
-       ,OFF ,OFF ,OFF ,OFF
-       ,OFF
-      ),
-
-    };
-#endif
-
     void
     configure (void) {
-
-#if WITH_PROGMEM_COLORMAP
-      Kaleidoscope.use (&ColormapEffect, NULL);
-
-      ColormapEffect.configure (colors, colorMap);
-      ColormapEffect.activate ();
-#else
       Kaleidoscope.use (&EEPROMColormapEffect, NULL);
+
       EEPROMColormapEffect.configure (LAYER_MAX);
       EEPROMColormapEffect.activate ();
 
-# if WITH_COLORMAP_FOCUS
       Focus.addHook (FOCUS_HOOK_COLORMAP);
-# endif
-#endif
     }
   };
 };
