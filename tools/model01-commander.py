6cb4e297b51f8958cb180326fe34af7540f9de97
Gergely Nagy <kaleidoscope@gergo.csillger.hu>
2017-10-19 23:08:47 +0200

    tools/model01-commander.py: Increase the Serial read timeout to 10s
    Signed-off-by: Gergely Nagy <kaleidoscope@gergo.csillger.hu>

--- tools/model01-commander.py
+++ tools/model01-commander.py
@@ -84,7 +84,7 @@ class Commander (object):
     _appSel = AppSel ()
 
     def run (self):
-        with serial.Serial ("/dev/ttyACM0", 9600, timeout = 1) as ser:
+        with serial.Serial ("/dev/ttyACM0", 9600, timeout = 10) as ser:
             while True:
                 cmd = ser.readline ().strip().decode()
                 if cmd.startswith ("appsel:"):
