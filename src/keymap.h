b255e02dc6d4decc27931fdf739630861d3374f0
Gergely Nagy <me@gergo.csillger.hu>
2018-11-08 10:03:09 +0100

    Add = to the numpad area
    This addresses the other part of #8. Also updates the colormap accordingly.
    
    Signed-off-by: Gergely Nagy <me@gergo.csillger.hu>

--- src/keymap.h
+++ src/keymap.h
@@ -147,7 +147,7 @@ KEYMAPS(
    ,OSL(_EMPTY)
 
    ,XXX            ,XXX        ,XXX   ,XXX   ,XXX   ,XXX       ,XXX
-   ,XXX            ,XXX        ,Key_1 ,Key_2 ,Key_3 ,XXX       ,XXX
+   ,XXX            ,XXX        ,Key_1 ,Key_2 ,Key_3 ,XXX       ,Key_Equals
                    ,Key_0      ,Key_4 ,Key_5 ,Key_6 ,Key_Minus ,Key_Plus
    ,Key_Underscore ,Key_Period ,Key_7 ,Key_8 ,Key_9 ,XXX       ,XXX
 
